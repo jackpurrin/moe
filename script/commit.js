90154632919798501c665fc3ed500b41e3cd9762
~autumn <jackpurrin@tutamail.com>
2025-05-26 15:58:18 -0700

    Create commit.js
    Signed-off-by: ~jack <me@jackpurrin.me>

---
+++ script/commit.js
@@ -0,0 +1,6 @@
+fetch('https://api.github.com/repos/jackpurrin/me/commits?per_page=1')
+  .then(res => res.json())
+  .then(res => {
+    document.getElementById('message').innerHTML = res[0].commit.message
+    document.getElementByID('date').innterHTML =  res[].commit.date
+  })
