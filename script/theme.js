7506adeac4025a9a29ef77e17b9a3e8ed5186493
~autumn <jackpurrin@tutamail.com>
2025-06-16 15:51:35 -0700

    i need to do the occasonal code formatting

--- script/theme.js
+++ script/theme.js
@@ -1,18 +1,17 @@
-// Get the root element
-const r = document.querySelector(':root');
-let theme = -10
+const r = document.querySelector(":root");
+let theme = -10;
 
-    if (localStorage.key(0) == null) {
-        theme = 0;
-    }
-    if (localStorage.key(0) != null) {
-        theme = localStorage.key(0) 
-    }
-    setTheme()
+if (localStorage.key(0) == null) {
+    theme = 0;
+}
+if (localStorage.key(0) != null) {
+    theme = localStorage.key(0);
+}
+setTheme();
 
 function changeTheme() {
     theme = theme + 1;
-    setTheme()
+    setTheme();
 }
 
 function setTheme() {
@@ -20,39 +19,38 @@ function setTheme() {
         theme = 0;
     }
     if (theme == 0) {
-        main()
+        main();
     }
     if (theme == 1) {
-        moon()
+        moon();
     }
     if (theme == 2) {
-        dawn()
+        dawn();
     }
-    localStorage.clear()
-    localStorage.setItem(theme, 0)
+    localStorage.clear();
+    localStorage.setItem(theme, 0);
 }
 
 function main() {
-  r.style.setProperty('--bkg-clr', '#191724');
-  r.style.setProperty('--lnk-clr', '#c4a7e7');
-  r.style.setProperty('--txt-clr', '#e0def4');
-  r.style.setProperty('--line-clr', '#524f67');
-  r.style.setProperty('--h2-clr', '#6e6a86');
+    r.style.setProperty("--bkg-clr", "#191724");
+    r.style.setProperty("--lnk-clr", "#c4a7e7");
+    r.style.setProperty("--txt-clr", "#e0def4");
+    r.style.setProperty("--line-clr", "#524f67");
+    r.style.setProperty("--h2-clr", "#6e6a86");
 }
 
 function moon() {
-  r.style.setProperty('--bkg-clr', '#232136');
-  r.style.setProperty('--lnk-clr', '#c4a7e7');
-  r.style.setProperty('--txt-clr', '#e0def4');
-  r.style.setProperty('--line-clr', '#56526e');
-  r.style.setProperty('--h2-clr', '#6e6a86');
+    r.style.setProperty("--bkg-clr", "#232136");
+    r.style.setProperty("--lnk-clr", "#c4a7e7");
+    r.style.setProperty("--txt-clr", "#e0def4");
+    r.style.setProperty("--line-clr", "#56526e");
+    r.style.setProperty("--h2-clr", "#6e6a86");
 }
 
 function dawn() {
-  r.style.setProperty('--bkg-clr', '#faf4ed');
-  r.style.setProperty('--lnk-clr', '#907aa9');
-  r.style.setProperty('--txt-clr', '#575279');
-  r.style.setProperty('--line-clr', '#cecacd');
-  r.style.setProperty('--h2-clr', '#9893a5');
+    r.style.setProperty("--bkg-clr", "#faf4ed");
+    r.style.setProperty("--lnk-clr", "#907aa9");
+    r.style.setProperty("--txt-clr", "#575279");
+    r.style.setProperty("--line-clr", "#cecacd");
+    r.style.setProperty("--h2-clr", "#9893a5");
 }
-
