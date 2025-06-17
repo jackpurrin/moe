7b82384ec3c8b2a7d94aec2ca06a10db9f5eaf93
~autumn <jackpurrin@tutamail.com>
2025-06-17 15:56:51 -0700

    makes the divs have two paragraph elements inside

--- script/changelog.js
+++ script/changelog.js
@@ -1,14 +1,17 @@
 divNumber = 0
 appendNumber = 0
 
-document.body.onload = addElement;
 function addElement() {
-  const newDiv = document.createElement("div")
-  newDiv.id = "d" + divNumber 
-  const newContent = document.createTextNode("d" + divNumber)
-  newDiv.appendChild(newContent)
+  const commitDiv = document.createElement("div")
+  commitDiv.id = "c" + divNumber 
+  const messageContent = document.createElement("p")
+  const dateContent = document.createElement("p")
+  commitDiv.appendChild(messageContent)
+  commitDiv.appendChild(dateContent)
+  messageContent.id = "m" + divNumber 
+  dateContent.id = "d" + divNumber 
   const currentDiv = document.getElementById("start")
-  currentDiv.after(newDiv) 
+  currentDiv.after(commitDiv) 
   divNumber++
   for (let divNumber = 1; divNumber < 10; divNumber++) {
     cloneElement()
@@ -16,29 +19,33 @@ function addElement() {
 }
 
 function cloneElement() {
-  const newDiv = document.createElement("div")
-  newDiv.id = "d" + divNumber 
-  const newContent = document.createTextNode("d" + divNumber)
-  newDiv.appendChild(newContent)
-  const currentDiv = document.getElementById("d" + appendNumber)
-  currentDiv.after(newDiv) 
+  const commitDiv = document.createElement("div")
+  commitDiv.id = "c" + divNumber 
+  const messageContent = document.createElement("p")
+  const dateContent = document.createElement("p")
+  commitDiv.appendChild(messageContent)
+  commitDiv.appendChild(dateContent)
+  messageContent.id = "m" + divNumber 
+  dateContent.id = "d" + divNumber 
+  const currentDiv = document.getElementById("c" + appendNumber)
+  currentDiv.after(commitDiv) 
   appendNumber++
   divNumber++
 }
 
-/*
+function getCommits() {
 fetch("https://api.github.com/repos/jackpurrin/me/commits?per_page=10")
     .then((res) => res.json())
     .then((res) => {
-        let fetchedMessage = res[resNum].commit.message;
+        let fetchedMessage = res[divNumber].commit.message;
         let replacedMessage = fetchedMessage.replace("Signed-off-by: ~jack", "");
-        document.getElementById("m"+resNum).innerHTML = replacedMessage;
+        document.getElementById("m"+divNumber).innerHTML = replacedMessage;
         let date = res[resNum].commit.author.date;
         let year = date.slice(0, 4);
         let month = date.slice(5, 7);
         let day = date.slice(8, 10);
         let hour = date.slice(11, 13);
         let min = date.slice(14, 16);
-        document.getElementById("d"+resNum).innerHTML = year + "-" + month + "-" + day + "\n" + hour + ":" + min + "";
+        document.getElementById("d"+divNumber).innerHTML = year + "-" + month + "-" + day + "\n" + hour + ":" + min + "";
     });
-*/
+}
