e6e6fc7a8fec8c3f643e14a5aaba21e7b6fd9246
~autumn <jackpurrin@tutamail.com>
2025-06-16 23:25:42 -0700

    formats the code

--- script/nekowebring.js
+++ script/nekowebring.js
@@ -1,54 +1,42 @@
-var tag = document.getElementById(ringID); //find the widget on the page
-
-thisSite = "https://jackpurrin.me"; //get the url of the site we're currently on
+var tag = document.getElementById(ringID);
+thisSite = "https://jackpurrin.me";
 thisIndex = null;
-
-// go through the site list to see if this site is on it and find its position
-for (i = 0; i < sites.length; i++) {
-  if (thisSite.startsWith(sites[i])) { //we use startswith so this will match any subdirectory, users can put the widget on multiple pages
-    thisIndex = i;
-    break; //when we've found the site, we don't need to search any more, so stop the loop
-  }
+for (i = 0; i < sites.length; i += 1) {
+    if (thisSite.startsWith(sites[i])) {
+        thisIndex = i;
+        break;
+    }
 }
-
 function randomSite() {
-  otherSites = sites.slice(); //create a copy of the sites list
-  otherSites.splice(thisIndex, 1); //remove the current site so we don't just land on it again
-  randomIndex = Math.floor(Math.random() * otherSites.length);
-  location.href = otherSites[randomIndex];
+    otherSites = sites.slice();
+    otherSites.splice(thisIndex, 1);
+    randomIndex = Math.floor(Math.random() * otherSites.length);
+    location.href = otherSites[randomIndex]
 }
-
-//if we didn't find the site in the list, the widget displays a warning instead
 if (thisIndex == null) {
-  tag.insertAdjacentHTML('afterbegin', `
+    tag.insertAdjacentHTML('afterbegin', `
 <table>
   <tr>
     <td>This site isn't part of the ${ringName} webring yet. You should talk to the manager to have your site added to the list!</td>
   </tr>
 </table>
-  `);
-}
-else {
-  //find the 'next' and 'previous' sites in the ring. this code looks complex
-  //because it's using a shorthand version of an if-else statement to make sure
-  //the first and last sites in the ring join together correctly
-  previousIndex = (thisIndex-1 < 0) ? sites.length-1 : thisIndex-1;
-  nextIndex = (thisIndex+1 >= sites.length) ? 0 : thisIndex+1;
-
-  indexText = ""
-  //if you've chosen to include an index, this builds the link to that
-  if (useIndex) {
-    indexText = `<a href='${indexPage}'>index</a> | `;
-  }
-
-  randomText = ""
-  //if you've chosen to include a random button, this builds the link that does that
-  if (useRandom) {
-    randomText = `<a href='javascript:void(0)' onclick='randomSite()'>random</a> | `;
-  }
-
-  //this is the code that displays the widget - EDIT THIS if you want to change the structure
-  tag.insertAdjacentHTML('afterbegin', `
+  `)
+} else {
+    previousIndex = (thisIndex - 1 < 0)
+        ? sites.length - 1
+        : thisIndex - 1;
+    nextIndex = (thisIndex + 1 >= sites.length)
+        ? 0
+        : thisIndex + 1;
+    indexText = "";
+    if (useIndex) {
+        indexText = `<a href='${indexPage}'>index</a> | `
+    }
+    randomText = "";
+    if (useRandom) {
+        randomText = `<a href='javascript:void(0)' onclick='randomSite()'>random</a> | `
+    }
+    tag.insertAdjacentHTML('afterbegin', `
                 <div class="ring">
                     <a href="${sites[previousIndex]}">←</a>
                     <a href="https://webring.nekoweb.org/members"><h2>nekowebring</h2></a>
@@ -56,7 +44,5 @@ else {
                     <br />
                     <a href="javascript:void(0)" onclick="randomSite()">↺</a>
                 </div>
-  `);
-
+  `)
 }
-
