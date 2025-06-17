951c3c57eb2ec016d46ba40e1a9a3b0c3461066f
~autumn <jackpurrin@tutamail.com>
2025-06-16 18:36:16 -0700

    readds all of my asset files, a CDN is not applicable with how bleeding edge my site is

---
+++ script/theme.js
@@ -0,0 +1,60 @@
+const r = document.querySelector(":root");
+let theme = -10;
+
+if (localStorage.key(0) == null) {
+    theme = 0;
+}
+if (localStorage.key(0) != null) {
+    theme = localStorage.key(0);
+}
+setTheme();
+
+function changeTheme() {
+    theme = theme + 1;
+    setTheme();
+}
+
+function setTheme() {
+    if (theme >= 3) {
+        theme = 0;
+    }
+    if (theme == 0) {
+        main();
+    }
+    if (theme == 1) {
+        moon();
+    }
+    if (theme == 2) {
+        dawn();
+    }
+    localStorage.clear();
+    localStorage.setItem(theme, 0);
+}
+
+function main() {
+    r.style.setProperty("--bkg-clr", "#191724");
+    r.style.setProperty("--lnk-clr", "#c4a7e7");
+    r.style.setProperty("--txt-clr", "#e0def4");
+    r.style.setProperty("--line-clr", "#524f67");
+    r.style.setProperty("--h2-clr", "#6e6a86");
+    r.style.setProperty("--cur-lnk-clr", "#eb6f92");
+
+}
+
+function moon() {
+    r.style.setProperty("--bkg-clr", "#232136");
+    r.style.setProperty("--lnk-clr", "#c4a7e7");
+    r.style.setProperty("--txt-clr", "#e0def4");
+    r.style.setProperty("--line-clr", "#56526e");
+    r.style.setProperty("--h2-clr", "#6e6a86");
+    r.style.setProperty("--cur-lnk-clr", "#eb6f92");
+}
+
+function dawn() {
+    r.style.setProperty("--bkg-clr", "#faf4ed");
+    r.style.setProperty("--lnk-clr", "#907aa9");
+    r.style.setProperty("--txt-clr", "#575279");
+    r.style.setProperty("--line-clr", "#cecacd");
+    r.style.setProperty("--h2-clr", "#9893a5");
+    r.style.setProperty("--cur-lnk-clr", "#b4637a");
+}
