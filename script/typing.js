50c57ef6cd444ce6c00c502adc66b6b86253c626
~autumn <jackpurrin@tutamail.com>
2025-07-24 11:50:38 -0700

    adds a typing animation to the homepage
    also adds some assets to be used later

---
+++ script/typing.js
@@ -0,0 +1,11 @@
+var i = 0;
+var txt = "jackpurrin's internet cafe"; /* The text */
+var speed = 50; /* The speed/duration of the effect in milliseconds */
+
+function typeWriter() {
+  if (i < txt.length) {
+    document.getElementById("type").innerHTML += txt.charAt(i);
+    i++;
+    setTimeout(typeWriter, speed);
+  }
+}
