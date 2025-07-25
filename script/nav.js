7422fd471223e4cf20072246bbf454bc38e017f7
~autumn <jackpurrin@tutamail.com>
2025-07-24 18:20:48 -0700

    cleanup time!
    you'll never beleive waht time it is, it's.... 18:20!!
    or also just like, code cleanup time or whatever

--- script/nav.js
+++ script/nav.js
@@ -1,3 +1,3 @@
 function hideshow() {
-  document.getElementById("navelements").classList.toggle("show");
+    document.getElementById("navelements").classList.toggle("show");
 }
--- script/typing.js
+++ script/typing.js
@@ -3,9 +3,9 @@ var txt = "jackpurrin's internet cafe"; /* The text */
 var speed = 50; /* The speed/duration of the effect in milliseconds */
 
 function typeWriter() {
-  if (i < txt.length) {
-    document.getElementById("type").innerHTML += txt.charAt(i);
-    i++;
-    setTimeout(typeWriter, speed);
-  }
+    if (i < txt.length) {
+        document.getElementById("type").innerHTML += txt.charAt(i);
+        i++;
+        setTimeout(typeWriter, speed);
+    }
 }
