c24afbcfacb911649e9a7c23da457df91735bd5b
~autumn <jackpurrin@tutamail.com>
2025-07-31 12:56:24 -0700

    changes the status to be a bit more clean

--- public/script/status.js
+++ public/script/status.js
@@ -6,6 +6,6 @@ fetch("https://status.cafe/users/jackpurrin/status.json")
             document.getElementById("statuscafe-content").innerHTML = "No status yet.";
             return;
         }
-        document.getElementById("statuscafe-username").innerHTML = '<a href="https://status.cafe/users/jackpurrin" target="_blank">' + r.author + "</a> " + r.face + " " + r.timeAgo;
-        document.getElementById("statuscafe-content").innerHTML = r.content;
+        document.getElementById("statuscafe-username").innerHTML = '<a class="name" href="https://status.cafe/users/jackpurrin" target="_blank">' + r.timeAgo + "</a> ";
+        document.getElementById("statuscafe-content").innerHTML =  r.face + " " + r.content;
     });
