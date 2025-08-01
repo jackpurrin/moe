4d78549d224f75fd60749e32e7a8415327a5e642
~autumn <jackpurrin@tutamail.com>
2025-08-01 14:17:23 -0700

    removes the now page to be replaced by a widget

--- public/script/widgets/status.js
+++ public/script/widgets/status.js
@@ -6,6 +6,5 @@ fetch("https://status.cafe/users/jackpurrin/status.json")
             document.getElementById("statuscafe-content").innerHTML = "No status yet.";
             return;
         }
-        document.getElementById("statuscafe-username").innerHTML = '<a class="name" href="https://status.cafe/users/jackpurrin" target="_blank">' + r.timeAgo + "</a> ";
-        document.getElementById("statuscafe-content").innerHTML =  r.face + " " + r.content;
+        document.getElementById("statuscafe-content").innerHTML = 'status: ' + r.content;
     });
