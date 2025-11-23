b4f7283b2871abaa4cfecbe92b96865b96832561
~autumn <jackpurrin@tutamail.com>
2025-11-23 10:45:40 -0700

    removes some useless js, cleans up the pages. etc.

--- public/script/widgets/domain.js
+++ public/script/widgets/domain.js
@@ -4,12 +4,12 @@ domainElement = document.getElementById("domain");
 function checkDomain() {
     if (webAddress.includes("file://")) {
         if (Math.random() >= 0.5) {
-            domainElement.innerHTML = "check out my main domain @ <a href='https://jackpurrin.me/'>jackpurrin.me</a>!";
+            domainElement.innerHTML = "check out my main domain @ <a href='https://jackpurrin.moe/'>jackpurrin.moe</a>!";
         } else {
             domainElement.innerHTML = "check out my nekoweb domain @ <a href='https://jackpurrin.nekoweb.org/'>nekoweb.jackpurrin.org</a>!";
         }
     } else if (webAddress.includes("nekoweb")) {
-        domainElement.innerHTML = "check out my main domain @ <a href='https://jackpurrin.me/'>jackpurrin.me</a>!";
+        domainElement.innerHTML = "check out my main domain @ <a href='https://jackpurrin.moe/'>jackpurrin.moe</a>!";
     } else if (webAddress.includes(".me")) {
         domainElement.innerHTML = "check out my nekoweb domain @ <a href='https://jackpurrin.nekoweb.org/'>nekoweb.jackpurrin.org</a>!";
     } else {
